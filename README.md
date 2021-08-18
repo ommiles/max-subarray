e6b8a0208336c9eb446a9cc8e5ee27fea9a4e77c
OM Miles <miles@ommiles.com>
2021-08-18 00:12:38 -0700

    solution added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# max-subarray
+# Maximum Subarray
+
+
+## Objective
+
+
+Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum contiguous subarray with the length of the number passed to the function.
--- solution.js
+++ solution.js
@@ -1,4 +1,27 @@
-// Given an array of integers and a number, 
+const maxSubArray = (arr, num) => {
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += arr[i];
+  }
+
+  let currentTotal = total;
+  for (let i = num; i < arr.length; i++) {
+    currentTotal += arr[i] - arr[i - num];
+    total = Math.max(total, currentTotal);
+  }
+
+  return total;
+};
+
+maxSubArray([100, 200, 300, 400], 2); // 700
+maxSubArray([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); // 39
+maxSubArray([-3, 4, 0, -2, 6, -1], 2); // 5
+maxSubArray([3, -2, 7, -4, 1, -1, 4, -2, 1], 2); // 5
+maxSubArray([2, 3], 3); // null
+
+// Given an array of integers and a number,
 // write a function called maxSubarraySum,
-// which finds the maximum sum of a subarray 
-// with the length of the number passed to the function. 
+// which finds the maximum sum contiguous subarray
+// with the length of the number passed to the function.
